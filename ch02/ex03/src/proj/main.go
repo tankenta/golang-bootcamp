9ff2d04be42b6fa1b28c2b4c8c353118216a5a3e
tankenta <hkenta112@gmail.com>
2021-09-17 09:39:06 +0900

    Add ch02/ex03

---
+++ ch02/ex03/src/proj/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"proj/popcount"
+)
+
+func main() {
+	var val uint64 = 3
+
+	start := time.Now()
+	pc := popcount.PopCount(val)
+	fmt.Printf("PopCount: %d, %d, %dns elapsed\n", val, pc, time.Since(start).Nanoseconds())
+
+	start = time.Now()
+	pc = popcount.LoopPopCount(val)
+	fmt.Printf("LoopPopCount: %d, %d, %dns elapsed\n", val, pc, time.Since(start).Nanoseconds())
+}
