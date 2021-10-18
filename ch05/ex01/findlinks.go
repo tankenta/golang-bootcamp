10e455fa52d8871b833688b47b3f43d6f92f1db1
tankenta <hkenta112@gmail.com>
2021-10-18 23:30:38 +0900

    Add ch05/ex01

---
+++ ch05/ex01/findlinks.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks: %v\n", err)
+		os.Exit(1)
+	}
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+}
+
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	c := n.FirstChild
+	if c != nil {
+		links = visit(links, c)
+	}
+	s := n.NextSibling
+	if s != nil {
+		links = visit(links, s)
+	}
+	return links
+}
